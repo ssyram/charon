fc0f4024106c66d4d321081dc075e733b4f23686
ssyram <liguany126@126.com>
2025-10-27 16:04:25 +0800

    debugging drop shim

--- charon/src/bin/charon-driver/translate/translate_trait_objects.rs
+++ charon/src/bin/charon-driver/translate/translate_trait_objects.rs
@@ -679,8 +679,23 @@ impl ItemTransCtx<'_, '_> {
                     });
                     ConstantExprKind::Ref(global)
                 }
-                // TODO(dyn): builtin impls
-                _ => ConstantExprKind::Opaque("missing supertrait vtable".into()),
+                hax::ImplExprAtom::Builtin { trait_data, impl_exprs, types } => {
+                    // TODO(dyn): handle builtin impls
+                    let _ = (trait_data, impl_exprs, types);
+                    ConstantExprKind::Opaque("missing supertrait vtable".into())
+                }
+                hax::ImplExprAtom::Dyn => {
+                    ConstantExprKind::Opaque("missing supertrait vtable: dyn".into())
+                }
+                hax::ImplExprAtom::Error(err) => {
+                    ConstantExprKind::Opaque(format!("missing supertrait vtable: error \"{}\"", err).into())
+                }
+                hax::ImplExprAtom::LocalBound { .. } => {
+                    ConstantExprKind::Opaque("missing supertrait vtable: generic local bound".into())
+                }
+                hax::ImplExprAtom::SelfImpl { .. } => {
+                    ConstantExprKind::Opaque("missing supertrait vtable: self impl".into())
+                }
             };
             mk_field(kind);
         }
@@ -955,8 +970,11 @@ impl ItemTransCtx<'_, '_> {
             arg_count: 1,
             locals: Vector::new(),
         };
+        
+        let _ = locals.new_var(Some("ret".into()), Ty::mk_unit());
         let dyn_self = locals.new_var(Some("dyn_self".into()), shim_receiver.clone());
         let target_self = locals.new_var(Some("target_self".into()), target_receiver.clone());
+        
         block.statements.push(Statement::new(
             span,
             StatementKind::Assign(
